39a8ea53a7622c25ac122bc4e1f4e71d4da5b42e
Nennneko5787 <nennneko5787@gmail.com>
2024-11-20 17:24:09 +0900

    HTML Living Dead

--- app/objects/board.py
+++ app/objects/board.py
@@ -12,5 +12,5 @@ class Board(BaseModel):
     name_count: int = Field(50)
     message_count: int = Field(2000)
     head: str = Field(
-        '<div style="text-align: center; margin: 1.2em 0">\n    <span style="color: red">クリックで救える命が…ないです(｀･ω･´)ｼｬｷｰﾝ</font>\n</div>\n\n<b>掲示板使用上の注意</b>\n<ul style="font-weight:bold;">\n    <li>･転んでも泣かない</li>\n    <li>･出されたものは残さず食べる</li>\n    <li>･Python使いを尊重する</li>\n</ul>'
+        '<div style="text-align: center; margin: 1.2em 0">\n    <span style="color: red">クリックで救える命が…ないです(｀･ω･´)ｼｬｷｰﾝ</span>\n</div>\n\n<b>掲示板使用上の注意</b>\n<ul style="font-weight:bold;">\n    <li>･転んでも泣かない</li>\n    <li>･出されたものは残さず食べる</li>\n    <li>･Python使いを尊重する</li>\n</ul>'
     )

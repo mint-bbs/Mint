e73d1cff98ad1d1d1300719ff23bdd6c7d505ab7
Nennneko5787 <nennneko5787@gmail.com>
2024-11-24 18:56:26 +0900

    Fix: 同一Ipのカウント

--- app/sioHandler.py
+++ app/sioHandler.py
@@ -1,8 +1,7 @@
 from collections import defaultdict
-
 import socketio
 
-from app.services.database import DatabaseService
+from .cloudflare import Cloudflare
 
 sio = socketio.AsyncServer(async_mode="asgi")
 
@@ -11,45 +10,52 @@ max_global_count = 0
 room_count: dict[int] = defaultdict(lambda: 0)
 room_max_count: dict[int] = defaultdict(lambda: 0)
 
+# Dictionary to track IPs and their associated session IDs
+ip_to_sid = {}
+
 
 @sio.event
 async def connect(sid, environ, auth):
     global global_count
     global max_global_count
-    global_count += 1
-    if global_count > max_global_count:
-        max_global_count = global_count
+
+    if (Cloudflare.isCloudflareIP(environ["REMOTE_ADDR"])) and (
+        "CF-Connecting-IP" in environ
+    ):
+        client_ip = environ["CF-Connecting-IP"]
+    elif "X_FORWARDED_FOR" in environ:
+        client_ip = environ["X_FORWARDED_FOR"]
+    else:
+        client_ip = environ["REMOTE_ADDR"]
+
+    if client_ip not in ip_to_sid:
+        ip_to_sid[client_ip] = sid
+        global_count += 1
+        if global_count > max_global_count:
+            max_global_count = global_count
+
     await sio.emit(
         "global_count_event",
         {"count": global_count, "max": max_global_count},
     )
-    print(f"connected", sid)
+    print(f"connected {sid}")
     print("connected member count", global_count)
-
-
-def get_sid_rooms(sid):
-    """
-    Get the rooms that a given sid is subscribed to.
-
-    Args:
-            sid: The SID of the client to get the rooms for.
-
-    Returns:
-            A set of room names.
-    """
-
-    rooms = set()
-    room_ids = sio.rooms(sid)
-    for room_id in room_ids:
-        rooms.add(room_id)
-
-    return rooms
 
 
 @sio.event
 async def disconnect(sid):
     global global_count
     global_count -= 1
+
+    client_ip = None
+    for ip, connected_sid in ip_to_sid.items():
+        if connected_sid == sid:
+            client_ip = ip
+            break
+
+    if client_ip:
+        del ip_to_sid[client_ip]
+
     await sio.emit(
         "global_count_event",
         {"count": global_count, "max": max_global_count},
@@ -61,7 +67,7 @@ async def disconnect(sid):
             {"count": room_count[room], "max": room_max_count[room]},
             room=room,
         )
-    print("disconnected", sid)
+    print(f"disconnected {sid}")
     print("connected member count", global_count)
 
 
@@ -76,5 +82,23 @@ async def join_room(sid, room):
         {"count": room_count[room], "max": room_max_count[room]},
         room=room,
     )
-    print("joinned", room)
+    print(f"joined {room}")
     print("connected member count", room_count[room])
+
+
+def get_sid_rooms(sid):
+    """
+    Get the rooms that a given sid is subscribed to.
+
+    Args:
+            sid: The SID of the client to get the rooms for.
+
+    Returns:
+            A set of room names.
+    """
+    rooms = set()
+    room_ids = sio.rooms(sid)
+    for room_id in room_ids:
+        rooms.add(room_id)
+
+    return rooms

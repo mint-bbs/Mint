dcbb846910c469199da81b65f09ed93bf9a813a5
Nennneko5787 <nennneko5787@gmail.com>
2024-12-08 19:30:16 +0900

    Fix: 接続人数に関わるバグを修正

--- app/sioHandler.py
+++ app/sioHandler.py
@@ -5,90 +5,74 @@ from .cloudflare import Cloudflare
 
 sio = socketio.AsyncServer(async_mode="asgi")
 
-global_count = 0
-max_global_count = 0
-room_count: dict[int] = defaultdict(lambda: 0)
-room_max_count: dict[int] = defaultdict(lambda: 0)
+globalList = list()
+maxGlobalCount = 0
+roomList: dict[list] = defaultdict(lambda: list())
+roomMaxCount: dict[int] = defaultdict(lambda: 0)
 
-# Dictionary to track IPs and their associated session IDs
-ip_to_sid = {}
+sidToAddr = {}
 
 
 @sio.event
 async def connect(sid, environ, auth):
-    global global_count
-    global max_global_count
+    global globalList
+    global maxGlobalCount
 
     if (Cloudflare.isCloudflareIP(environ["REMOTE_ADDR"])) and (
         "HTTP_CF_CONNECTING_IP" in environ
     ):
-        client_ip = environ["HTTP_CF_CONNECTING_IP"]
+        clientAddr = environ["HTTP_CF_CONNECTING_IP"]
     elif "HTTP_X_FORWARDED_FOR" in environ:
-        client_ip = environ["HTTP_X_FORWARDED_FOR"]
+        clientAddr = environ["HTTP_X_FORWARDED_FOR"]
     else:
-        client_ip = environ["REMOTE_ADDR"]
+        clientAddr = environ["REMOTE_ADDR"]
 
-    print(client_ip)
+    sidToAddr[sid] = clientAddr
 
-    if client_ip not in ip_to_sid:
-        ip_to_sid[client_ip] = sid
-        global_count += 1
-        if global_count > max_global_count:
-            max_global_count = global_count
+    globalList.append(clientAddr)
+    if len(set(globalList)) > maxGlobalCount:
+        maxGlobalCount = len(globalList)
 
     await sio.emit(
         "global_count_event",
-        {"count": global_count, "max": max_global_count},
+        {"count": len(set(globalList)), "max": maxGlobalCount},
     )
-    print(f"connected {sid}")
-    print("connected member count", global_count)
 
 
 @sio.event
 async def disconnect(sid):
-    global global_count
-    client_ip = None
-    for ip, connected_sid in ip_to_sid.items():
-        if connected_sid == sid:
-            client_ip = ip
-            break
+    global globalList
+    clientAddr = sidToAddr[sid]
 
-    if client_ip:
-        del ip_to_sid[client_ip]
+    del clientAddr[sid]
 
-    if client_ip not in ip_to_sid:
-        if global_count > 0:
-            global_count -= 1
+    globalList.remove(clientAddr)
 
     await sio.emit(
         "global_count_event",
-        {"count": global_count, "max": max_global_count},
+        {"count": len(set(globalList)), "max": maxGlobalCount},
     )
     for room in get_sid_rooms(sid):
-        if room_count[room] > 0:
-            room_count[room] -= 1
+        roomList[room].remove(clientAddr)
         await sio.emit(
             "count_event",
-            {"count": room_count[room], "max": room_max_count[room]},
+            {"count": len(set(roomList[room])), "max": roomMaxCount[room]},
             room=room,
         )
-    print(f"disconnected {sid}")
-    print("connected member count", global_count)
 
 
 @sio.event
 async def join_room(sid, room):
     await sio.enter_room(sid, room)
-    room_count[room] += 1
-    if room_count[room] > room_max_count[room]:
-        room_max_count[room] = room_count[room]
+    clientAddr = sidToAddr[sid]
+    roomList[room].append(clientAddr)
+    if len(set(roomList[room])) > roomMaxCount[room]:
+        roomMaxCount[room] = len(set(roomList[room]))
     await sio.emit(
         "count_event",
-        {"count": room_count[room], "max": room_max_count[room]},
+        {"count": len(set(roomList[room])), "max": roomMaxCount[room]},
         room=room,
     )
-    print(f"joined {room}")
-    print("connected member count", room_count[room])
 
 
 def get_sid_rooms(sid):

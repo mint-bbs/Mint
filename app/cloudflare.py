607369ab904000abeea78a8ec1e8ed35aab89cb9
ねんねこ <nennneko5787@gmail.com>
2024-12-09 22:55:22 +0900

    cloudflare.py を更新

--- app/cloudflare.py
+++ app/cloudflare.py
@@ -2,12 +2,17 @@ import ipaddress
 
 
 class Cloudflare:
-    addressList = []
+    addressListv4 = []
+    addressListv6 = []
 
     @classmethod
     def isCloudflareIP(cls, ip: str):
-        for ipRange in cls.addressList:
+        for ipRange in cls.addressListv4:
             network = ipaddress.IPv4Network(ipRange, strict=False)
+            if ipaddress.ip_address(ip) in network:
+                return True
+        for ipRange in cls.addressListv6:
+            network = ipaddress.IPv6Network(ipRange, strict=False)
             if ipaddress.ip_address(ip) in network:
                 return True
         return False
